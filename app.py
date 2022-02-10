d4f2d7ea6e8d5f94fc72e9d40f24c3ef8ff212a3
Matheus Souza Ribeiro <matheus.souzaribeiro@gmail.com>
2022-02-10 10:29:48 -0300

    state

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<CPF>/")
 def index(CPF=None):
-    belongs = "RUNNING"
+    state = "RUNNING"
     if CPF:
         def format_cpf(c): return re.sub('[^0-9]', '', c)
         cpf_formated = format_cpf(CPF)
-        belongs = "FREE"
+        state = "FREE"
         with open("./blacklist.txt", "r") as blacklist:
             for cpf in blacklist:
                 if format_cpf(cpf) == cpf_formated:
-                    belongs = "BLOCK"
+                    state = "BLOCK"
                     break
 
-    return render_template("index.html",  result=belongs)
+    return render_template("index.html",  result=state)
 
 if __name__ == "__main__":
     app.run(debug=True)
